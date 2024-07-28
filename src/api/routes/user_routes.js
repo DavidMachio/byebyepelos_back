f226df24c7e7247b90c8ec7f927f6e5b9615274a
david.machio <david.machio@MB-07.local>
2024-07-28 14:29:43 +0200

    primer commit

--- src/api/routes/user_routes.js
+++ src/api/routes/user_routes.js
@@ -3,7 +3,7 @@ const {checkUser} = require('../../middlewares/checkUser');
 const { register, getUsers, getUser, login, updateUser, deleteUser } = require('../controllers/user_controllers');
 
 const userRouter = require('express').Router();
-
+//routes
 
 userRouter.post("/register", register)
 userRouter.post('/login', login)
@@ -11,5 +11,7 @@ userRouter.put('/:id', isAuth, checkUser, updateUser)
 userRouter.delete('/:id', isAdmin, deleteUser)
 userRouter.get("/", getUsers)
 userRouter.get("/:name", getUser)
+
+
 
 module.exports = userRouter;
